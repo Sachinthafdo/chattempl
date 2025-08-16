0fd2b2d88723ce499388799f5db9b06a711eb8af
sachintha fernando <sachinthafernando2004@gmail.com>
2025-08-16 12:18:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+import { ChatProvider  } from "@/components/chat-provider"
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -25,7 +26,10 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body> 
+    <ChatProvider>
+      {children}
+    </ChatProvider></body>
     </html>
   )
 }
